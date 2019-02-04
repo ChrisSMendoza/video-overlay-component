0b530413ccf16982c5d0d3818fbd51a329ed19d1
mendoza1 <csmendoza@ucdavis.edu>
2019-02-03 18:16:07 -0800

    main content component added

---
+++ main-content.js
@@ -0,0 +1,26 @@
+// main-content.js
+
+Vue.component('main-content', {
+
+	template: `
+		<section class="row justify-content-center h-100 text-white">
+
+			<div class="col-lg-6 col-sm-12 h-100">
+
+				<div class="d-flex flex-column justify-content-around h-100">
+
+					<h1>A city unmatched.</h1>
+
+					<h1>Where aerial shots look this good.</h1>
+
+					<button type="button" class="btn btn-primary w-100">
+						Show me the way
+					</button>
+
+				</div>
+
+			</div>
+
+		</section>
+	`
+});
